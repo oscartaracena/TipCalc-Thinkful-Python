f908bc671c206b70fb81c27aa5d570deb2169ce6
Oscar Taracena <oscar.taracena@gmail.com>
2014-03-16 18:02:42 -0700

    added some more logic
    cleaned up the code a little, added some statements

--- TipCalculator.py
+++ TipCalculator.py
@@ -13,13 +13,17 @@ tip = 0.15 #tip that will be added to the bill
 print "\n\n\n"
 
 #input of the amount of the meal
-print "Hello and please enter the total you spend on dinner:$ "
+print "Hello and please enter the total you spend on dinner: "
 totalCostofMeal = float(raw_input())
+
 print "\n\n"
+print "$" + str(totalCostofMeal) 
 #The math and calculations 
 mealWithTax = (totalCostofMeal * tax)
 
 total = mealWithTax + totalCostofMeal 
 total1 = total * tip
-print str(total) + " which line is this?" 
+print str(total) + " Cost of dinner and tax" 
+print "\n"
 print str(total + total1) + " Is the total cost of meal with tax and tip."
+print "\n"
