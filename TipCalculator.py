fecd860f5b388eab7b1d121a10979b7812c128f2
Oscar Taracena <oscar.taracena@gmail.com>
2014-03-16 17:32:19 -0700

    testing the math and input
    Testing input and printing the amount. Note no formatting is being done
    here yet.

--- TipCalculator.py
+++ TipCalculator.py
@@ -5,3 +5,15 @@ add tax and calculate the tip from the total. """
 #Oscar Taracena
 #TipCalculator.py - Ver. 1.0
 #03.16.2014
+
+#variables 
+
+tax = 0.009 #tax that will be charged for the meal
+tip = 0.15 #tip that will be added to the bill
+
+print "Hello and please enter the total you spend on dinner: "
+totalCostofMeal = raw_input()
+
+mealWithTax = totalCostofMeal * tax
+
+print mealWithTax
