7125abe7d428c9a5d900c536d966cdcca3a0dd1a
Oscar Taracena <oscar.taracena@gmail.com>
2014-03-16 17:23:55 -0700

    Tip Calculator
    Tip Calculator

---
+++ TipCalculator.py
@@ -0,0 +1,7 @@
+"""Tip Calculator Program in python
+where this will take an amount entered with a
+add tax and calculate the tip from the total. """
+
+#Oscar Taracena
+#TipCalculator.py - Ver. 1.0
+#03.16.2014
